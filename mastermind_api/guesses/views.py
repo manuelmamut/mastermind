4317901dd3761846bcee9845091f1156debc12b1
Mamut MAC <manuelj.fuentesg@gmail.com>
2018-07-09 17:12:33 -0400

    Forgot to add guesse views so it can accept POST request

--- mastermind_api/guesses/views.py
+++ mastermind_api/guesses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render
+from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -8,6 +8,8 @@ from guesses.models import Guess
 from guesses.serializers import GuessesSerializer
 
 
+@authentication_classes([])
+@permission_classes([])
 class GuessesList(generics.ListCreateAPIView):
     """\brief, Class used to handle Game models listing and creation"""
     queryset = Guess.objects.all()
